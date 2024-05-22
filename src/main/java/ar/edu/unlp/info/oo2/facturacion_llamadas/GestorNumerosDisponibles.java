2303af7b76d6af724949375eeb3808c7719ed11b
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-05-21 21:58:53 -0300

    formatear código

--- src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/GestorNumerosDisponibles.java
+++ src/main/java/ar/edu/unlp/info/oo2/facturacion_llamadas/GestorNumerosDisponibles.java
@@ -6,34 +6,33 @@ import java.util.Random;
 import java.util.SortedSet;
 
 public class GestorNumerosDisponibles {
-	private SortedSet<String> lineas = new TreeSet<String>();
-	private String tipoGenerador = "ultimo";
+    private SortedSet<String> lineas = new TreeSet<String>();
+    private String tipoGenerador = "ultimo";
 
-	public SortedSet<String> getLineas() {
-		return lineas;
-	}
+    public SortedSet<String> getLineas() {
+        return lineas;
+    }
 
-	public String obtenerNumeroLibre() {
-		String linea;
-		switch (tipoGenerador) {
-			case "ultimo":
-				linea = lineas.last();
-				lineas.remove(linea);
-				return linea;
-			case "primero":
-				linea = lineas.first();
-				lineas.remove(linea);
-				return linea;
-			case "random":
-				linea = new ArrayList<String>(lineas)
-						.get(new Random().nextInt(lineas.size()));
-				lineas.remove(linea);
-				return linea;
-		}
-		return null;
-	}
+    public String obtenerNumeroLibre() {
+        String linea;
+        switch (tipoGenerador) {
+            case "ultimo":
+                linea = lineas.last();
+                lineas.remove(linea);
+                return linea;
+            case "primero":
+                linea = lineas.first();
+                lineas.remove(linea);
+                return linea;
+            case "random":
+                linea = new ArrayList<String>(lineas).get(new Random().nextInt(lineas.size()));
+                lineas.remove(linea);
+                return linea;
+        }
+        return null;
+    }
 
-	public void cambiarTipoGenerador(String valor) {
-		this.tipoGenerador = valor;
-	}
+    public void cambiarTipoGenerador(String valor) {
+        this.tipoGenerador = valor;
+    }
 }
